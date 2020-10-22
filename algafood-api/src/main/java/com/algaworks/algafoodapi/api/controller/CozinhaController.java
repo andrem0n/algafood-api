f5003650bd65ab6737469b8b5f893474255470be
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-10-22 20:57:29 -0300

    usando response entity

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -4,8 +4,13 @@ import com.algaworks.algafoodapi.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -21,14 +26,15 @@ public class CozinhaController {
         return cozinhaRepository.todas();
     }
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public Cozinha findById(@PathVariable Long id) {
-        return cozinhaRepository.porId(id);
-    }
-
     @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-    public CozinhasXmlWrapper listarXml(){
+    public CozinhasXmlWrapper listarXml() {
         return new CozinhasXmlWrapper(cozinhaRepository.todas());
     }
+
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Cozinha> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(cozinhaRepository.porId(id));
+    }
+
 
 }
