fd9dba1a36c429189c2735278e29e6152cc87462
André Bolzan Egert <andre.bolzan@ntconsult.com.br>
2020-10-27 21:36:56 -0300

    implementação metodo http remover

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -53,6 +54,20 @@ public class CozinhaController {
             return ResponseEntity.ok(cozinhaRepository.adicionar(cozinhaAtual));
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cozinha> remover(@PathVariable Long id) {
+        try {
+            Cozinha cozinhaAtual = cozinhaRepository.porId(id);
+            if (Objects.nonNull(cozinhaAtual)) {
+                cozinhaRepository.remover(cozinhaAtual);
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.notFound().build();
+        } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
 }
