70b2531561803a57a278acc84f48e1aa14130d2c
andrem0n <andreegert75@gmail.com>
2021-09-09 23:28:08 -0300

    teste git

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Cidade.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Cidade.java
@@ -22,6 +22,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cidade {
 
+  //teste git
   @EqualsAndHashCode.Include
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
